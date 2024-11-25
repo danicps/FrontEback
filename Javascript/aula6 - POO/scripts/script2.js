b911a129642075a2cd282c02bfbea3abb97895f6
danicps <ddanielle.campos@gmail.com>
2024-11-25 17:41:10 -0300

    aula6
    update

--- Javascript/aula6 - POO/scripts/script2.js
+++ Javascript/aula6 - POO/scripts/script2.js
@@ -1,6 +1,6 @@
 class Usuario {
 //   id, @, email, bio, gênero
-constructor(id, nickname, nome, email, bio, genero){
+constructor(id, apelido, nome, email, bio, genero){
     this.id = id;
     this.apelido = apelido;
     this.nome = nome;
@@ -10,15 +10,28 @@ constructor(id, nickname, nome, email, bio, genero){
     this.postar = function(pensamento){
         document.querySelector("#resposta").innerHTML =
         `
-      <div id="post">
+      <div class="post">
       <h3>${this.apelido}</h3>
       <p>${pensamento} </p>
       </div>
         
         `
    
-     }
-   } 
-}
+       }
+    } 
+ }
 
 var user1 = new Usuario(1, 'dani', 'Danielle', 'dani@gmail.com', 'estudante', 'feminino')
+var user2 = new Usuario(2, 'dani2', 'Daniellezinha', 'dani@gmail.com', 'professor', 'feminino')
+
+function postarFeed(){
+    alert("POSTADO COM SUCESSO!")
+    pensamento = document.querySelector('#pensamento').value
+
+    user1.postar(pensamento)
+}
+
+function carregarPerfil(){
+    
+
+}
