cd08d8439108c1a19d142ab5685a9337327d0a7d
danicps <ddanielle.campos@gmail.com>
2025-03-18 17:35:17 -0300

    up
    up

--- carrinho/index.php
+++ carrinho/index.php
@@ -3,21 +3,48 @@
 //incia sessão
 session_start();
 
-//verifica se carrinho está null
+class Produto{
+    public $id;
+    public $nome;
+    public $valor;
+    public $desc;
+
+    public function _construct($id,$nome,$valor,$desc){
+        $this->$id = $id;
+        $this->$nome = $nome;
+        $this->$valor = $valor;
+        $this->$desc = $desc;
+
+    }
+}
+
+// public - qualquer parte do código mexe no atributo
+// private - apenas o que está dentro do class
+
+//verifica se algo está null
 if(!isset($_SESSION['carrinho'])){
     $_SESSION['carrinho'] = [];
 }
 
 //adiciona um produto
 if(isset($_GET['produto'])){
-    // NOMEVARIAVEL[INDICEVAZIO] = PRODUTO -> PODE SER ADICIONADO 
-    // SUBSTITUIDO DO +=
+    $produtosDados = new Produto($_GET['id'],$_GET['produto'],$_GET['valor'],$_GET['desc']);
+    $_SESSION['carrinho'][] = $produtosDados;
+}
+
+// limpar carrinho
+if (isset($_GET['LimparCarrinho'])){
     $_SESSION['carrinho'] = [];
 }
 
-if(isset($_GET['deletarCarrinho'])){
-    // session_destroy();
-    $_SESSION['carrinho'] = [];
+// remover um item
+if(isset($_GET['removerItem'])){
+    foreach($_SESSION['carrinho'] as $produto){
+        if($produto->id == $_GET['removerItem']){
+            unset($_SESSION['carrinho'][$produto]);
+            $_SESSION['carrinho'] = array_values($_SESSION['carrinho']);
+        }
+    }
 }
 
 // var_dump($_SESSION['carrinho']);
@@ -29,28 +56,46 @@ if(isset($_GET['deletarCarrinho'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carrinho</title>
+    <title>Carrinho de Compras</title>
     <link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
 
     <h1>Carrinho de Compras</h1>
-<p><a href="?produto=SSD">Adicionar SSD</a></p>
-<p><a href="?produto=Disco de memória">Adicionar Disco de memória</a></p>
-<p><a href="?produto=Memória RAM">Adicionar Memória RAM</a></p>
-<p><a href="?produto=Placa mãe">Adicionar Placa mãe</a></p>
+<p><a href="?id=1&produto=Carmed&valor=20,00&desc='Brilho rosa e cheirinho de fini sem igual'">Adicionar Carmed</a></p>
+<p><a href="?id=2&produto=Pó Compacto&valor=50,00&desc='Efeito matte prolongado'">Adicionar Pó Compacto</a></p>
+<p><a href="?id=3&produto=Gloss Natura&valor=105,99&desc='com cheirinho de Tutti Frutti e hidratação para os lábios'">Adicionar Gloss</a></p>
+<p><a href="?id=4&produto=Blush&valor=99,99&desc='efeito profissional com diversos tons'">Adicionar blush</a></p>
+<a href="?LimparCarrinho=True">Limpar carrinho</a>
 
-<!-- deletar -->
-<p><a href="?deletarCarrinho=True">Deletar Carrinho</a></p>
+<?php
+$valorTotal = 0;
+foreach($_SESSION['carrinho'] as $produto){
+    $valorTotal += $produto->valor;
+}
 
+if($valorTotal == 0){
+    echo "O carrinho está vazio";
+}else{
+    echo "O valor total R$ $valorTotal";
+}
+
+?>
+
+<h2>Produtos no Carrinho</h2>
 <ol>
     <?php
     // foreach (array as alias){}
     // foreach (filmes as filmes){filme.nome, filme.desc}
     foreach ($_SESSION['carrinho'] as $produto){
-        echo "<li> $produto </li>";
+        echo "<li>
+        <h1> $produto->nome </h1>
+        <p> R$ $produto->nome </p>
+        <p> $produto->desc </p>
+        </li>";
     } 
-    ?>
+
+ ?>
 
  </ol>
 </body>
